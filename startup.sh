d7705d674599a4114048ae1b0bf9753d57182d22
suchao <suchao@renrenche.com>
2019-02-20 15:04:08 +0800

    删除

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-filename=`basename "$0"`
-workspace=$(cd $(dirname $0) && pwd)
-nginx -c $workspace/nginx-testing.conf
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-filename=`basename "$0"`
-workspace=$(cd $(dirname $0) && pwd)
-nginx -s stop -c $workspace/nginx-testing.conf
